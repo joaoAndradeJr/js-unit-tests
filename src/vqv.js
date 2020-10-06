336de8ef10ab8ad6cf911b1d5db4c714c6c3704b
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-06 14:30:17 -0400

    CC

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,10 @@
 
 const vqv = (nome, idade) => {
   if (nome !== undefined && idade !== undefined) {
-    return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+    return `  Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   }
   return undefined;
 };

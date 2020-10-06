5a40d76f0bd6cdb4a938a4805c11091c8b28b961
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-06 14:23:46 -0400

    CC

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome != undefined && idade != undefined) {
+  if (nome !== undefined && idade !== undefined) {
     return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
   }
+  return undefined;
 };
 
 module.exports = vqv;

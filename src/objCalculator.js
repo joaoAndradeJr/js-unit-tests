9d22a9ed4cae045949ec29567d43d0287368c49b
Joao Andrade Junior <fullservicemanaus@gmail.com>
2020-10-06 10:46:07 -0400

    correçao linhas 25 e 26

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -22,8 +22,8 @@
 const calculator = {
   add: (num1, num2) => num1 + num2,
   mult: (num1, num2) => num1 * num2,
-  div: (num1, num2) => num1 / num2,
-  sub: (num1, num2) => parseInt(num1 / num2, 10),
+  div: (num1, num2) => parseInt(num1 / num2, 10),
+  sub: (num1, num2) => num1 - num2,
 };
 
 module.exports = calculator;
